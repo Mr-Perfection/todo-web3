6ba371e7f79986cb0258d84718089acace691988
Stephen Lee <sungsoolee0127@gmail.com>
2022-12-17 22:34:09 +0000

    Get the contract address after the deployment.

---
+++ frontend/src/constants.ts
@@ -0,0 +1,3 @@
+// Get it from etherscan.io
+// i.e. https://sepolia.etherscan.io/address/0xac9fcba56e42d5960f813b9d0387f3d3bc003338
+export const TaskContractAddress = "0xaC9fCBA56E42d5960f813B9D0387F3D3bC003338"
