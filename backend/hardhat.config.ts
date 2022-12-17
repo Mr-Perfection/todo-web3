b48cf72b5081d19bcec402aeb83d00364cac025d
Stephen Lee <sungsoolee0127@gmail.com>
2022-12-17 01:57:15 +0000

    GOERLI will be deprecated soon - use Sepolia instead. https://github.com/eth-clients/sepolia

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -8,7 +8,7 @@ const config: HardhatUserConfig = {
   solidity: "0.8.17",
   networks: {
     sepolia: {
-      url: process.env.ALCHEMY_GOERLI_URL,
+      url: process.env.SEPOLIA_URL,
       accounts: [process.env.ACCOUNT_PRIVATE_KEY || ""]
     }
   }
